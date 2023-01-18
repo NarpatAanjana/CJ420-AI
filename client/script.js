c4b90693b460d4364d1196f9450b5dbe02f10d19
narpat <narpatanjana0@gmail.com>
2023-01-18 23:14:24 +0530

    Url Update

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async function (e) {
   loader(messageDiv);
 
   // fect data from server <bot>
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://nu1194.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
